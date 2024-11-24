2f891d9d540d4953ccbc4eda347990d4931dae05
Dennis <dennissung6105@gmail.com>
2024-11-24 13:20:13 +1000

    Added setup and piece class

--- newchess.py
+++ newchess.py
@@ -1,5 +1,9 @@
+from typing import List
+
+# Colour as int
 WHITE = 1
 BLACK = 0
+# Piece Name
 KNIGHT = "knight"
 ROOK = "rook"
 QUEEN = "queen"
@@ -8,7 +12,7 @@ BISHOP = "bishop"
 PAWN = "pawn"
 
 # Piece class
-class Piece:
+class Piece():
     def __init__(self, name: str, position: tuple[int, int], colour: int):
         self.name = name
         self.position = position
@@ -23,12 +27,29 @@ class Piece:
     def get_colour(self) -> int:
         return self.colour
     
+    def print_colour(self) -> str:
+        if self.colour:
+            return WHITE
+        return BLACK
+    
     def change_position(self, new_position: tuple[int, int]):
         self.position = new_position
+    
+    def to_chess_notation(self) -> str:
+        x, y = self.position
+        file = chr(x + ord('a'))
+        rank = (1 + y)
+        return file + rank
+
+    def __repr__(self) -> str:
+        return "Piece({name}, {position}, {colour})\n".format(name=self.get_name(), position=self.get_position(), colour=self.get_colour())
+
+    def __str__(self) -> str:
+        return "{colour} {piece} is on {position}".format(colour=self.print_colour(), piece=self.get_name(), position=self.to_chess_notation())
 
 # The logic behind the controller
-class Move:
-    def __init__(self, piece, start, end, captured=None, special=None):
+class Move():
+    def __init__(self, piece: Piece, start, end, captured=None, special=None):
         self.piece = piece
         self.start = start
         self.end = end
@@ -49,15 +70,46 @@ class Move:
     
     def get_special(self):
         return self.special
+    
+    def change_piece_position(self, new_position: tuple[int, int]):
+        self.piece.change_position(new_position)
 
 # The controller that controls Move and GUI
 class Chess():
     def __init__(self):
         self.move_history = []
+        self.pieces = self.setup()
         self.turn = "white"
 
     def play(self):
+        print(self.move_history)
+        print(self.pieces)
         return
+    
+    """
+        Hard coded the pieces in to make it more easily readable and understandable
+    """
+    def setup(self) -> List[Piece]:
+        return [# BLACK PIECES
+                Piece(ROOK, (0, 0), BLACK),
+                Piece(KNIGHT, (1, 0), BLACK),
+                Piece(BISHOP, (2, 0), BLACK),
+                Piece(QUEEN, (3, 0), BLACK),
+                Piece(KING, (4, 0), BLACK),
+                Piece(BISHOP, (5, 0), BLACK),
+                Piece(KNIGHT, (6, 0), BLACK),
+                Piece(ROOK, (7, 0), BLACK)
+                ] + [Piece(PAWN, (i, 1), BLACK) for i in range(8)] + \
+                [# WHITE PIECES
+                Piece(ROOK, (0, 7), WHITE),
+                Piece(KNIGHT, (1, 7), WHITE),
+                Piece(BISHOP, (2, 7), WHITE),
+                Piece(QUEEN, (3, 7), WHITE),
+                Piece(KING, (4, 7), WHITE),
+                Piece(BISHOP, (5, 7), WHITE),
+                Piece(KNIGHT, (6, 7), WHITE),
+                Piece(ROOK, (7, 7), WHITE)
+                ] + [Piece(PAWN, (i, 7), WHITE) for i in range(8)]
     
     def make_move(self, piece, start, end, captured=None, special=None):
         move = Move(piece, start, end, captured, special)
@@ -69,7 +121,14 @@ class Chess():
             self.revert_move(last_move)
 
     def revert_move(self, last_move: Move):
-        last_move.piece.position = last_move.start
+        # Change the position of the last move piece
+        last_move.piece.change_position(last_move.get_start())
+        # Restore any captured piece
+        if last_move.get_captured():
+            self.place_piece(last_move.get_captured())
+
+    def place_piece(self, captured: Piece):
+        self.pieces.append(captured)
 
 def main():
     chess = Chess()
